de1a9ac5222c82d1a61c0ddf4bc5e1a684689e3c
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-19 17:42:34 +0200

    Implement basic cube rotation system

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::{
     reflect::TypePath,
     render::render_resource::{AsBindGroup, ShaderRef},
 };
+use cgmath::{Matrix4, Rad, Vector3};
 use iyes_perf_ui::prelude::*;
 
 fn main() {
@@ -19,9 +20,13 @@ fn main() {
         .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
         .add_plugins(PerfUiPlugin)
+        .add_systems(Update, animate)
         .add_systems(Startup, setup)
         .run();
 }
+
+#[derive(Component)]
+struct MyCube;
 
 fn setup(
     mut commands: Commands,
@@ -30,12 +35,19 @@ fn setup(
 ) {
     commands.spawn(PerfUiCompleteBundle::default());
 
-    commands.spawn((MaterialMeshBundle {
-        mesh: meshes.add(Cuboid::default()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        material: materials.add(CustomMaterial {}),
-        ..default()
-    },));
+    let mesh = meshes.add(Cuboid::default());
+    let transform = Transform::from_xyz(0.0, 0.0, 0.0);
+    let material = materials.add(CustomMaterial {});
+
+    commands.spawn((
+        MaterialMeshBundle {
+            mesh,
+            transform,
+            material,
+            ..default()
+        },
+        MyCube,
+    ));
 
     // Transform for the camera and lighting, looking at (0,0,0) (the position of the mesh).
     let camera_and_light_transform =
@@ -54,5 +66,13 @@ struct CustomMaterial {}
 impl Material for CustomMaterial {
     fn fragment_shader() -> ShaderRef {
         "shaders/animate_shader.wgsl".into()
+    }
+}
+
+fn animate(time: Res<Time>, mut query: Query<(&mut Transform, &MyCube)>) {
+    let rotation_change = time.delta_seconds() * 5.0;
+
+    for (mut transform, cube) in query.iter_mut() {
+        transform.rotate(Quat::from_rotation_y(Rad(rotation_change).0));
     }
 }
