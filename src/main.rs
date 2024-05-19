5bd275365745b66d0130cef64ea93542628c85ab
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-19 18:21:46 +0200

    Add basic input controller

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use bevy::{
     prelude::*,
     reflect::TypePath,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::{
+        camera,
+        render_resource::{AsBindGroup, ShaderRef},
+    },
+    window::WindowResolution,
 };
 use cgmath::{Matrix4, Rad, Vector3};
 use iyes_perf_ui::prelude::*;
@@ -10,6 +14,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
+                resolution: WindowResolution::new(720.0, 720.0),
                 present_mode: bevy::window::PresentMode::Immediate,
                 ..default()
             }),
@@ -20,7 +25,7 @@ fn main() {
         .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
         .add_plugins(PerfUiPlugin)
-        .add_systems(Update, animate)
+        .add_systems(Update, (animate, camera_controller))
         .add_systems(Startup, setup)
         .run();
 }
@@ -35,23 +40,32 @@ fn setup(
 ) {
     commands.spawn(PerfUiCompleteBundle::default());
 
-    let mesh = meshes.add(Cuboid::default());
-    let transform = Transform::from_xyz(0.0, 0.0, 0.0);
-    let material = materials.add(CustomMaterial {});
+    let chunk_size = 38;
 
-    commands.spawn((
-        MaterialMeshBundle {
-            mesh,
-            transform,
-            material,
-            ..default()
-        },
-        MyCube,
-    ));
+    for x in 0..chunk_size {
+        for z in 0..chunk_size {
+            let mesh = meshes.add(Cuboid::default());
+            let transform = Transform::from_xyz(
+                (x - chunk_size / 2) as f32,
+                0.0,
+                (z - chunk_size / 2) as f32,
+            );
+            let material = materials.add(CustomMaterial {});
 
-    // Transform for the camera and lighting, looking at (0,0,0) (the position of the mesh).
+            commands.spawn((
+                MaterialMeshBundle {
+                    mesh,
+                    transform,
+                    material,
+                    ..default()
+                },
+                MyCube,
+            ));
+        }
+    }
+
     let camera_and_light_transform =
-        Transform::from_xyz(1.8, 1.8, 1.8).looking_at(Vec3::ZERO, Vec3::Y);
+        Transform::from_xyz(0.0, 50.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y);
 
     // Camera in 3D space.
     commands.spawn(Camera3dBundle {
@@ -74,5 +88,41 @@ fn animate(time: Res<Time>, mut query: Query<(&mut Transform, &MyCube)>) {
 
     for (mut transform, cube) in query.iter_mut() {
         transform.rotate(Quat::from_rotation_y(Rad(rotation_change).0));
+    }
+}
+
+fn camera_controller(
+    time: Res<Time>,
+    keys: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &Camera)>,
+) {
+    for (mut transform, camera) in query.iter_mut() {
+        let mut translation = Vec3::ZERO;
+
+        if keys.pressed(KeyCode::KeyW) {
+            translation += Vec3::X;
+        }
+        if keys.pressed(KeyCode::KeyA) {
+            translation -= Vec3::Z;
+        }
+        if keys.pressed(KeyCode::KeyS) {
+            translation -= Vec3::X;
+        }
+        if keys.pressed(KeyCode::KeyD) {
+            translation += Vec3::Z;
+        }
+
+        if keys.pressed(KeyCode::Space) {
+            translation += Vec3::Y;
+        }
+
+        if keys.pressed(KeyCode::ShiftLeft) || keys.pressed(KeyCode::ShiftRight) {
+            translation -= Vec3::Y;
+        }
+
+        if translation != Vec3::ZERO {
+            let translation = translation.normalize() * 4.0 * time.delta_seconds();
+            transform.translation += translation;
+        }
     }
 }
