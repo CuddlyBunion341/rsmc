3b4cfed73a1b7f90f04d4d843d89fda20dbf65df
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-20 15:42:01 +0200

    Fix raycasting

--- src/main.rs
+++ src/main.rs
@@ -88,11 +88,16 @@ fn setup(mut commands: Commands) {
 
 const RAY_DIST: Vec3 = Vec3::new(0.0, 0.0, -7.0);
 
-fn raycast(mut raycast: Raycast, mut gizmos: Gizmos, time: Res<Time>) {
-    let t = time.elapsed_seconds();
-    let pos = Vec3::new(t.sin(), (t * 1.5).cos() * 2.0, t.cos()) * 1.5 + RAY_DIST;
-    let dir = (RAY_DIST - pos).normalize();
-    // This is all that is needed to raycast into the world! You can also use the normal, non-debug
-    // version (raycast.cast_ray) when you don't need to visualize the ray or intersections.
-    raycast.debug_cast_ray(Ray3d::new(pos, dir), &default(), &mut gizmos);
+// query camera position and direction
+fn raycast(
+    mut raycast: Raycast,
+    mut gizmos: Gizmos,
+    query: Query<&Transform, With<FpsCameraController>>,
+) {
+    for transform in query.iter() {
+        let pos = transform.translation;
+        let dir = transform.rotation.mul_vec3(Vec3::Z).normalize();
+        let dir = dir * RAY_DIST.z;
+        raycast.debug_cast_ray(Ray3d::new(pos, dir), &default(), &mut gizmos);
+    }
 }
