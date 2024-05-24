0e9d7187a608fbc7f06f9f22341d3149c9e6ff0b
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-25 00:00:44 +0200

    Readd old code

---
+++ src/controller.rs
@@ -0,0 +1,85 @@
+use std::f32::consts::TAU;
+
+use bevy::{prelude::*, render::camera::Exposure, window::CursorGrabMode};
+use bevy_rapier3d::prelude::*;
+
+use bevy_fps_controller::controller::*;
+
+const SPAWN_POINT: Vec3 = Vec3::new(0.0, 40.0, 0.0);
+
+pub fn setup_controller(mut commands: Commands, mut window: Query<&mut Window>) {
+    let mut window = window.single_mut();
+    window.title = String::from("Minimal FPS Controller Example");
+
+    let logical_entity = commands
+        .spawn((
+            Collider::capsule(Vec3::Y * 0.5, Vec3::Y * 1.5, 0.5),
+            Friction {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            Restitution {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            ActiveEvents::COLLISION_EVENTS,
+            Velocity::zero(),
+            RigidBody::Dynamic,
+            Sleeping::disabled(),
+            LockedAxes::ROTATION_LOCKED,
+            AdditionalMassProperties::Mass(1.0),
+            GravityScale(0.0),
+            Ccd { enabled: true }, // Prevent clipping when going fast
+            TransformBundle::from_transform(Transform::from_translation(SPAWN_POINT)),
+            LogicalPlayer,
+            FpsControllerInput {
+                pitch: -TAU / 12.0,
+                yaw: TAU * 5.0 / 8.0,
+                ..default()
+            },
+            FpsController {
+                air_acceleration: 80.0,
+                ..default()
+            },
+        ))
+        .insert(CameraConfig {
+            height_offset: 0.0,
+            radius_scale: 0.75,
+        })
+        .id();
+
+    commands.spawn((
+        Camera3dBundle {
+            projection: Projection::Perspective(PerspectiveProjection {
+                fov: TAU / 5.0,
+                ..default()
+            }),
+            exposure: Exposure::SUNLIGHT,
+            ..default()
+        },
+        RenderPlayer { logical_entity },
+    ));
+}
+
+pub fn manage_cursor(
+    btn: Res<ButtonInput<MouseButton>>,
+    key: Res<ButtonInput<KeyCode>>,
+    mut window_query: Query<&mut Window>,
+    mut controller_query: Query<&mut FpsController>,
+) {
+    let mut window = window_query.single_mut();
+    if btn.just_pressed(MouseButton::Left) {
+        window.cursor.grab_mode = CursorGrabMode::Locked;
+        window.cursor.visible = false;
+        for mut controller in &mut controller_query {
+            controller.enable_input = true;
+        }
+    }
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor.grab_mode = CursorGrabMode::None;
+        window.cursor.visible = true;
+        for mut controller in &mut controller_query {
+            controller.enable_input = false;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use bevy::{
     prelude::*,
     window::WindowResolution,
 };
+use bevy_fps_controller::controller::FpsControllerPlugin;
 use bevy_rapier3d::{
-    plugin::{NoUserData, RapierPhysicsPlugin},
+    plugin::{NoUserData, RapierConfiguration, RapierPhysicsPlugin, TimestepMode},
     render::RapierDebugRenderPlugin,
 };
 use chunk_manager::ChunkManager;
+use controller::{manage_cursor, setup_controller};
 use input::{
     handle_block_update_events, handle_chunk_mesh_update_events, handle_fps_controller_input,
     handle_keyboard_events, handle_mouse_events, BlockUpdateEvent, ChunkMeshUpdateEvent,
@@ -23,6 +25,7 @@ use world::setup_world;
 mod blocks;
 mod chunk;
 mod chunk_manager;
+mod controller;
 mod generator;
 mod input;
 mod mesher;
@@ -45,14 +48,24 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
         )
-        // .add_plugins(LookTransformPlugin)
-        // .add_plugins(FpsCameraPlugin::default())
         .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
+        .insert_resource(RapierConfiguration {
+            gravity: Vec3::new(0., -1.6, 0.),
+            physics_pipeline_active: true,
+            force_update_from_transform_changes: false,
+            scaled_shape_subdivision: 1,
+            query_pipeline_active: true,
+            timestep_mode: TimestepMode::Fixed {
+                dt: 1.0 / 120.0,
+                substeps: 1,
+            },
+        })
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(PerfUiPlugin)
+        .add_plugins(FpsControllerPlugin)
         .insert_resource(ChunkManager::new())
         .insert_resource(SelectedPosition(None))
         .insert_resource(SelectedNormal(None))
@@ -63,7 +76,13 @@ fn main() {
         .insert_resource(LastPlayerPosition(Vec3::ZERO))
         .add_systems(
             Startup,
-            (setup, setup_world, add_highlight_cube, add_coliders),
+            (
+                setup,
+                setup_world,
+                add_highlight_cube,
+                add_coliders,
+                setup_controller,
+            ),
         )
         .add_systems(
             Update,
@@ -75,6 +94,7 @@ fn main() {
                 handle_keyboard_events,
                 handle_collider_update_events,
                 handle_fps_controller_input,
+                manage_cursor,
             ),
         )
         .add_event::<BlockUpdateEvent>()
