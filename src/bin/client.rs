c3225217db588f2fb7bce894645e396fbaf75d39
Daniel Bengl <daniel.bengl@renuo.ch>
2024-06-07 18:29:57 +0200

    Implement basic 3d client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,26 @@
-use bevy::app::{App, Update};
-use bevy::ecs::system::ResMut;
+use bevy::app::{App, Startup, Update};
+use bevy::asset::{Assets, Handle};
+use bevy::core_pipeline::core_3d::{Camera3d, Camera3dBundle};
+use bevy::ecs::component::Component;
+use bevy::ecs::entity::Entity;
+use bevy::ecs::event::EventReader;
+use bevy::ecs::query::With;
+use bevy::ecs::system::{Command, Commands, Query, Res, ResMut, Resource};
+use bevy::input::keyboard::{KeyCode, KeyboardInput};
+use bevy::input::ButtonState;
 use bevy::log::info;
-use bevy::utils::SystemTime;
+use bevy::math::primitives::{Cuboid, Plane3d};
+use bevy::math::Vec3;
+use bevy::pbr::{AmbientLight, MaterialMeshBundle, StandardMaterial};
+use bevy::prelude::default;
+use bevy::reflect::Reflect;
+use bevy::render::color::Color;
+use bevy::render::mesh::shape::{Cube, Plane};
+use bevy::render::mesh::Mesh;
+use bevy::transform::components::Transform;
+use bevy::utils::{HashMap, SystemTime};
 use bevy::DefaultPlugins;
+use bevy_fps_controller::controller::FpsControllerInput;
 use bevy_renet::transport::NetcodeClientPlugin;
 use bevy_renet::RenetClientPlugin;
 use rand;
@@ -10,25 +28,45 @@ use renet::transport::{ClientAuthentication, NetcodeClientTransport};
 use renet::{ConnectionConfig, DefaultChannel, RenetClient};
 use std::net::{SocketAddrV4, UdpSocket};
 
+#[derive(Resource)]
+pub struct PlayerInput {
+    pub left: bool,
+    pub right: bool,
+    pub forward: bool,
+    pub backward: bool,
+}
+
+#[derive(Resource)]
+pub struct PlayerLobby(HashMap<u64, Transform>);
+
+#[derive(Component)]
+pub struct MyPlayer;
+
 fn main() {
     let mut app = App::new();
     app.add_plugins(RenetClientPlugin);
 
     let client = RenetClient::new(ConnectionConfig::default());
     app.insert_resource(client);
+    app.insert_resource(PlayerInput {
+        left: false,
+        right: false,
+        forward: false,
+        backward: false,
+    });
 
     // Setup the transport layer
     app.add_plugins(NetcodeClientPlugin);
     app.add_plugins(DefaultPlugins);
 
-    let random_int = rand::random::<u64>();
+    let client_id = rand::random::<u64>();
 
     let authentication = ClientAuthentication::Unsecure {
         server_addr: std::net::SocketAddr::V4(SocketAddrV4::new(
             std::net::Ipv4Addr::new(127, 0, 0, 1),
             5000,
         )),
-        client_id: random_int,
+        client_id,
         user_data: None,
         protocol_id: 0,
     };
@@ -39,13 +77,75 @@ fn main() {
     let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
 
     app.insert_resource(transport);
+    app.insert_resource(PlayerLobby(HashMap::default()));
 
     app.add_systems(Update, send_message_system);
     app.add_systems(Update, receive_message_system);
+    app.add_systems(Update, handle_keyboard_input);
+    app.add_systems(Update, update_player_movement);
+    app.add_systems(Startup, setup);
 
-    info!("Starting client {}", random_int);
+    info!("Starting client {}", client_id);
 
     app.run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh_transform = Transform::from_xyz(0.0, 0.0, 0.0);
+
+    let mesh = MaterialMeshBundle {
+        mesh: meshes.add(Cuboid::default()),
+        transform: mesh_transform,
+        material: materials.add(StandardMaterial {
+            emissive: Color::rgb(255.0, 255.0, 0.0),
+            ..default()
+        }),
+        ..default()
+    };
+
+    commands.spawn((mesh, MyPlayer));
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(3.0, 3.0, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn handle_keyboard_input(
+    mut keyboard_events: EventReader<KeyboardInput>,
+    mut input: ResMut<PlayerInput>,
+) {
+    for event in keyboard_events.read() {
+        let state = event.state == ButtonState::Pressed;
+        match event.key_code {
+            KeyCode::KeyW => input.forward = state,
+            KeyCode::KeyS => input.backward = state,
+            KeyCode::KeyD => input.right = state,
+            KeyCode::KeyA => input.left = state,
+            _ => {}
+        }
+    }
+}
+
+fn update_player_movement(
+    input: ResMut<PlayerInput>,
+    mut query: Query<(Entity, &mut Transform), With<MyPlayer>>,
+) {
+    let (_, mut player_transform) = query.single_mut();
+
+    let deltatime = 0.016;
+    let speed = 2.0;
+
+    let movement = deltatime * speed;
+
+    player_transform.translation.x += if input.right { movement } else { 0.0 };
+    player_transform.translation.x -= if input.left { movement } else { 0.0 };
+    player_transform.translation.z += if input.forward { movement } else { 0.0 };
+    player_transform.translation.z -= if input.backward { movement } else { 0.0 };
 }
 
 // Systems
