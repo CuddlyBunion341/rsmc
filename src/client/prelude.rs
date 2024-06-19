c4e44301de00d0b45da0e453838c596f8bee4574
Daniel Bengl <daniel.bengl@renuo.ch>
2024-06-19 07:38:41 +0200

    Refactor prelude imports

--- src/client/prelude.rs
+++ src/client/prelude.rs
@@ -14,50 +14,41 @@ pub use crate::{
     },
 };
 
-pub use bevy::app::*;
-pub use bevy::app::{App, Plugin, Startup, Update};
-pub use bevy::diagnostic::*;
-pub use bevy::math::*;
-pub use bevy::prelude::*;
-pub use bevy::render::camera::Camera;
-pub use bevy::window::*;
 pub use bevy::{
     asset::Assets,
-    ecs::{query::Without, system::ResMut},
-    gizmos::gizmos::Gizmos,
-    math::{primitives::Cuboid, Ray3d, Vec3},
-    pbr::{PbrBundle, StandardMaterial},
-    render::{color::Color, mesh::Mesh},
-};
-pub use bevy::{
-    ecs::system::Commands,
-    math::{EulerRot, Quat},
-    pbr::{light_consts, CascadeShadowConfigBuilder, DirectionalLight, DirectionalLightBundle},
-    prelude::default,
-    transform::components::Transform,
-};
-pub use bevy::{
-    ecs::{event::*, query::With, system::Query},
-    input::keyboard::*,
-};
-pub use bevy::{
+    diagnostic::*,
     ecs::{
-        event::{EventReader, EventWriter},
-        system::Res,
+        event::*, query::With, query::Without, system::Commands, system::Query, system::Res,
+        system::ResMut,
     },
+    gizmos::gizmos::Gizmos,
     input::{
-        keyboard::KeyCode,
+        keyboard::*,
         mouse::{MouseButton, MouseButtonInput},
         ButtonInput,
     },
-    window::{CursorGrabMode, Window},
+    math::{primitives::Cuboid, EulerRot, Quat, Ray3d, Vec3},
+    pbr::{
+        light_consts, CascadeShadowConfigBuilder, DirectionalLight, DirectionalLightBundle,
+        PbrBundle, StandardMaterial,
+    },
+    prelude::*,
+    render::{camera::*, color::Color, mesh::Mesh},
+    transform::components::Transform,
+    window::{CursorGrabMode, Window, *},
 };
 pub use bevy_fps_controller::controller::FpsController;
 pub use bevy_fps_controller::controller::FpsControllerPlugin;
 pub use bevy_fps_controller::controller::*;
 pub use bevy_mod_raycast::immediate::{Raycast, RaycastSettings};
 pub use bevy_rapier3d::geometry::Collider;
-pub use bevy_rapier3d::prelude::*;
+pub use bevy_rapier3d::{
+    dynamics::{
+        AdditionalMassProperties, Ccd, CoefficientCombineRule, GravityScale, LockedAxes, RigidBody,
+        Sleeping, Velocity,
+    },
+    geometry::{ActiveEvents, Friction, Restitution},
+};
 pub use bevy_rapier3d::{
     plugin::{NoUserData, RapierConfiguration, RapierPhysicsPlugin, TimestepMode},
     render::RapierDebugRenderPlugin,
