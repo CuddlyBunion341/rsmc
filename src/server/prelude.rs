cccbb33537d1136341f5484eec81f256685bfc16
Daniel Bengl <daniel.bengl@renuo.ch>
2024-06-19 20:17:00 +0200

    Refactor server imports

--- src/server/prelude.rs
+++ src/server/prelude.rs
@@ -1,13 +1,13 @@
 pub use crate::networking::systems as networking_systems;
 
-pub use bevy::app::*;
-pub use bevy::app::{App, Update};
-pub use bevy::ecs::system::ResMut;
-pub use bevy::MinimalPlugins;
-pub use bevy_renet::{transport::NetcodeServerPlugin, RenetServerPlugin};
-pub use renet::DefaultChannel;
-pub use renet::{
-    transport::{NetcodeServerTransport, ServerAuthentication, ServerConfig},
-    ConnectionConfig, RenetServer,
-};
 pub use std::{net::UdpSocket, time::SystemTime};
+
+pub use bevy::app::*;
+pub use bevy::ecs::system::*;
+pub use bevy::MinimalPlugins;
+
+pub use bevy_renet::transport::*;
+pub use bevy_renet::RenetServerPlugin;
+pub use renet::transport::*;
+pub use renet::DefaultChannel;
+pub use renet::*;
