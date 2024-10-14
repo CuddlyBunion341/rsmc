78ae5834b22ad979edde588323f6959ee61a812a
Daniel Bengl <daniel@bengl.cz>
2024-10-14 21:02:36 +0200

    Fix clippy

--- src/client/player/systems/keyboard.rs
+++ src/client/player/systems/keyboard.rs
@@ -6,17 +6,12 @@ pub fn handle_keyboard_events_system(
     mut collider_events: EventWriter<collider_events::ColliderUpdateEvent>,
 ) {
     for event in keyboard_events.read() {
-        if event.state.is_pressed() {
-            match event.key_code {
-                bevy::input::keyboard::KeyCode::KeyC => {
-                    let controller_transform = camera_query.single();
-                    println!("Handling event: {:?}", controller_transform.translation);
-                    collider_events.send(collider_events::ColliderUpdateEvent {
-                        position: controller_transform.translation.into(),
-                    });
-                }
-                _ => {}
-            }
+        if event.state.is_pressed() && event.key_code == bevy::input::keyboard::KeyCode::KeyC {
+            let controller_transform = camera_query.single();
+            println!("Handling event: {:?}", controller_transform.translation);
+            collider_events.send(collider_events::ColliderUpdateEvent {
+                position: controller_transform.translation.into(),
+            });
         }
     }
 }
