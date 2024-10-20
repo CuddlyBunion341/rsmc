0f60a37c91a20d1c0edda4bfbdb392227bc6586f
Daniel Bengl <daniel@bengl.cz>
2024-10-20 12:35:05 +0200

    Implement terrain resource tests

--- src/client/terrain/util/chunk.rs
+++ src/client/terrain/util/chunk.rs
@@ -5,7 +5,8 @@ pub const PADDED_CHUNK_SIZE: usize = CHUNK_SIZE + 2;
 pub const PADDED_CHUNK_USIZE: usize = PADDED_CHUNK_SIZE;
 pub const CHUNK_LENGTH: usize = PADDED_CHUNK_SIZE * PADDED_CHUNK_SIZE * PADDED_CHUNK_SIZE;
 
-pub struct Chunk {
+#[derive(Clone)]
+ pub struct Chunk {
     pub data: [BlockId; CHUNK_LENGTH],
     pub position: Vec3,
 }
