aad5605cb284df363deef83e7bf9388dcd2ab43b
Daniel Bengl <daniel@bengl.cz>
2024-11-05 23:12:29 +0100

    Attempt update channel configuration

--- src/client/networking/mod.rs
+++ src/client/networking/mod.rs
@@ -1,5 +1,7 @@
 pub mod systems;
 
+use renet::{ChannelConfig, SendType};
+
 use crate::prelude::*;
 
 const SERVER_ADDR: &str = "127.0.0.1:5000";
@@ -9,7 +11,36 @@ impl Plugin for NetworkingPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((RenetClientPlugin, NetcodeClientPlugin));
 
-        let client = RenetClient::new(ConnectionConfig::default());
+        let channel_config_unreliable = ChannelConfig {
+            channel_id: 0,
+            max_memory_usage_bytes: 1000 * 1024 * 1024,
+            send_type: SendType::Unreliable,
+        };
+
+        let channel_config_reliable_ordered = ChannelConfig {
+            channel_id: 1,
+            max_memory_usage_bytes: 1000 * 1024 * 1024,
+            send_type: SendType::ReliableOrdered {
+                resend_time: Duration::from_millis(300),
+            },
+        };
+
+        let channel_config_reliable_unordered = ChannelConfig {
+            channel_id: 2,
+            max_memory_usage_bytes: 1000 * 1024 * 1024,
+            send_type: SendType::ReliableUnordered {
+                resend_time: Duration::from_millis(300),
+            },
+        };
+
+        let client = RenetClient::new(ConnectionConfig {
+            client_channels_config: Vec::from([
+                channel_config_unreliable,
+                channel_config_reliable_ordered,
+                channel_config_reliable_unordered,
+            ]),
+            ..Default::default()
+        });
         app.insert_resource(client);
 
         let client_id = rand::random::<u64>();
--- src/server/networking/mod.rs
+++ src/server/networking/mod.rs
@@ -12,7 +12,37 @@ impl Plugin for NetworkingPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(RenetServerPlugin);
 
-        let server = RenetServer::new(ConnectionConfig::default());
+        let channel_config_unreliable = ChannelConfig {
+            channel_id: 0,
+            max_memory_usage_bytes: 1000 * 1024 * 1024,
+            send_type: SendType::Unreliable,
+        };
+
+        let channel_config_reliable_ordered = ChannelConfig {
+            channel_id: 1,
+            max_memory_usage_bytes: 1000 * 1024 * 1024,
+            send_type: SendType::ReliableOrdered {
+                resend_time: Duration::from_millis(300),
+            },
+        };
+
+        let channel_config_reliable_unordered = ChannelConfig {
+            channel_id: 2,
+            max_memory_usage_bytes: 1000 * 1024 * 1024,
+            send_type: SendType::ReliableUnordered {
+                resend_time: Duration::from_millis(300),
+            },
+        };
+
+        let server = RenetServer::new(ConnectionConfig {
+            server_channels_config: Vec::from([
+                channel_config_unreliable,
+                channel_config_reliable_ordered,
+                channel_config_reliable_unordered,
+            ]),
+            ..Default::default()
+        });
+
         app.insert_resource(server);
 
         app.add_plugins(NetcodeServerPlugin);
@@ -29,32 +59,6 @@ impl Plugin for NetworkingPlugin {
         };
         let transport = NetcodeServerTransport::new(server_config, socket).unwrap();
         app.insert_resource(transport);
-
-        let send_type_unreliable = SendType::Unreliable;
-        let send_type_reliable_ordered = SendType::ReliableOrdered {
-            resend_time: Duration::from_millis(300),
-        };
-        let send_type_reliable_unordered = SendType::ReliableUnordered {
-            resend_time: Duration::from_millis(300),
-        };
-
-        let channel_config_unreliable = ChannelConfig {
-            channel_id: 0,
-            max_memory_usage_bytes: 5 * 1024 * 1024, // 5 megabytes
-            send_type: send_type_unreliable,
-        };
-
-        let channel_config_reliable_ordered = ChannelConfig {
-            channel_id: 1,
-            max_memory_usage_bytes: 5 * 1024 * 1024, // 5 megabytes
-            send_type: send_type_reliable_ordered,
-        };
-
-        let channel_config_reliable_unordered = ChannelConfig {
-            channel_id: 2,
-            max_memory_usage_bytes: 5 * 1024 * 1024, // 5 megabytes
-            send_type: send_type_reliable_unordered,
-        };
 
         app.add_systems(Update, networking_systems::receive_message_system);
         app.add_systems(Update, networking_systems::handle_events_system);
