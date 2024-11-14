cdb3bf3eb322f2629d7ccddd00587ed92b9f060e
Daniel Bengl <daniel.bengl@renuo.ch>
2024-11-14 20:14:17 +0100

    Fix merge conflicts

--- src/client/terrain/util/mod.rs
+++ src/client/terrain/util/mod.rs
@@ -1,15 +1,9 @@
 pub mod blocks;
 pub mod buffer_serializer;
 pub mod chunk;
-pub mod generator;
-pub mod chunk;
-pub mod generator;
 pub mod mesher;
 
 pub use blocks::*;
 pub use buffer_serializer::*;
 pub use chunk::*;
-pub use generator::*;
-pub use chunk::*;
-pub use generator::*;
 pub use mesher::*;
