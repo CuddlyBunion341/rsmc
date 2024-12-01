7acaf674459e50028983e13332b60d67b4ea2454
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-01 19:49:21 +0100

    Remove button

--- src/client/gui/systems.rs
+++ src/client/gui/systems.rs
@@ -37,36 +37,5 @@ pub fn setup_gui_system(mut commands: Commands, asset_server: Res<AssetServer>) 
                 ),
                 ..Default::default()
             });
-            // "RSMC - Pre Alpha",
-            // TextStyle {
-            //     font: asset_server.load("fonts/Terminus500.ttf"),
-            //     font_size: 60.0,
-            //     color: Color::rgb(0.9, 0.9, 0.9),
-            // },
-            // ));
-
-            parent
-                .spawn(ButtonBundle {
-                    style: Style {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    border_color: BorderColor(Color::BLACK),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(TextBundle::from_section(
-                        "Button",
-                        TextStyle {
-                            font: asset_server.load("fonts/Terminus500.ttf"),
-                            font_size: 40.0,
-                            color: Color::rgb(0.9, 0.9, 0.9),
-                        },
-                    ));
-                });
         });
 }
