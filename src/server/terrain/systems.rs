cf39446dc0a3a726d5ce520aa44aae0823534cbe
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-01 18:19:50 +0100

    Improve logging

--- src/server/terrain/systems.rs
+++ src/server/terrain/systems.rs
@@ -5,12 +5,15 @@ pub fn setup_world_system(mut chunk_manager: ResMut<terrain_resources::ChunkMana
 
     let render_distance = 16;
 
+    info!("Generating chunks");
+
     let mut chunks = terrain_resources::ChunkManager::instantiate_chunks(
         Vec3::new(0.0, 0.0, 0.0),
         render_distance,
     );
 
     for chunk in &mut chunks {
+        info!("Generating chunk at {:?}", chunk.position);
         generator.generate_chunk(chunk);
     }
 
