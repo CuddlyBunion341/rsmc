e0123c858ad1e08c244fc64e0f5d542f6775c420
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-01 19:48:42 +0100

    Update style

--- src/client/gui/systems.rs
+++ src/client/gui/systems.rs
@@ -13,21 +13,37 @@ pub fn setup_gui_system(mut commands: Commands, asset_server: Res<AssetServer>) 
             style: Style {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
+                align_items: AlignItems::FlexStart,
+                justify_content: JustifyContent::FlexStart,
+                padding: UiRect {
+                    left: Val::Px(5.0),
+                    right: Val::Px(5.0),
+                    top: Val::Px(5.0),
+                    bottom: Val::Px(5.0),
+                },
                 ..default()
             },
             ..default()
         })
         .with_children(|parent| {
-            parent.spawn(TextBundle::from_section(
-                "RSMC - Pre Alpha",
-                TextStyle {
-                    font: asset_server.load("fonts/Terminus500.ttf"),
-                    font_size: 60.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            ));
+            parent.spawn(TextBundle {
+                text: Text::from_section(
+                    "RSMC - Pre Alpha",
+                    TextStyle {
+                        font: asset_server.load("fonts/Terminus500.ttf"),
+                        font_size: 60.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                ),
+                ..Default::default()
+            });
+            // "RSMC - Pre Alpha",
+            // TextStyle {
+            //     font: asset_server.load("fonts/Terminus500.ttf"),
+            //     font_size: 60.0,
+            //     color: Color::rgb(0.9, 0.9, 0.9),
+            // },
+            // ));
 
             parent
                 .spawn(ButtonBundle {
