a4950b787acd59f5fe733142435422de5f0b20f7
Daniel Bengl <daniel.bengl@renuo.ch>
2025-01-12 17:04:44 +0100

    Update entrypoint

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,17 +1,21 @@
 pub mod chat;
+pub mod dashboard;
 pub mod networking;
 pub mod player;
 pub mod prelude;
 pub mod terrain;
-pub mod dashboard;
 
 use bevy_log::LogPlugin;
+use bevy_tui::{prelude::{initialize_terminal, teardown_terminal}, MinimalTuiPlugins};
 
 use crate::prelude::*;
 
 fn main() {
+    let _ = initialize_terminal();
+
     let mut app = App::new();
     app.add_plugins(MinimalPlugins);
+    app.add_plugins(MinimalTuiPlugins);
     app.add_plugins(LogPlugin::default());
     app.add_plugins(player::PlayerPlugin);
     app.add_plugins(networking::NetworkingPlugin);
@@ -19,4 +23,6 @@ fn main() {
     app.add_plugins(chat::ChatPlugin);
     app.add_plugins(dashboard::DashboardPlugin);
     app.run();
+
+    let _ = teardown_terminal();
 }
