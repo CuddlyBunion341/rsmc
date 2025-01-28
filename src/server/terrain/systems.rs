275b17dcef0605ce68dde6e5a0a9333ba193bb2a
Daniel Bengl <daniel.bengl@renuo.ch>
2025-01-28 23:28:04 +0100

    Increase server render distance

--- src/server/terrain/systems.rs
+++ src/server/terrain/systems.rs
@@ -3,11 +3,11 @@ use crate::prelude::*;
 pub fn setup_world_system(mut chunk_manager: ResMut<ChunkManager>) {
     let generator = terrain_util::generator::Generator::new(0);
 
-    let render_distance = Vec3::new(2.0, 2.0, 2.0);
+    let render_distance = Vec3::new(12.0, 2.0, 12.0);
 
     info!("Generating chunks");
 
-    let mut chunks = ChunkManager::instantiate_chunks(Vec3::new(0.0, 0.0, 0.0), render_distance);
+    let mut chunks = ChunkManager::instantiate_chunks(Vec3::ZERO, render_distance);
 
     chunks.par_iter_mut().for_each(|chunk| {
         info!("Generating chunk at {:?}", chunk.position);
