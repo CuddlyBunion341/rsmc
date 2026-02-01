c7ebbed1cb8b61d5db3357b6f166b0e43a36943d
Daniel Bengl <daniel.bengl@renuo.ch>
2026-02-01 19:49:45 +0100

    Make world generation deterministic

--- src/server/terrain/persistence.rs
+++ src/server/terrain/persistence.rs
@@ -179,7 +179,6 @@ mod tests {
         let mut actual_new_chunk = Chunk::new(IVec3::new(20, 0, 20));
         generator.generate_chunk(&mut actual_new_chunk);
 
-        // FIXME: Make Terrain Generation fully deterministic
-        // assert_eq!(possible_new_chunk.data, actual_new_chunk.data);
+        assert_eq!(possible_new_chunk.data, actual_new_chunk.data);
     }
 }
