94942c87b6a83718c4fbd713d19cbfc02cecdafb
Daniel Bengl <daniel.bengl@renuo.ch>
2026-02-15 12:01:32 +0100

    Reduce height

--- src/shared/chunk/constants.rs
+++ src/shared/chunk/constants.rs
@@ -1,4 +1,4 @@
 pub const CHUNK_SIZE: usize = 32;
-pub const CHUNK_HEIGHT: usize = 256;
+pub const CHUNK_HEIGHT: usize = 128;
 pub const PADDED_CHUNK_SIZE: usize = CHUNK_SIZE + 2;
 pub const CHUNK_LENGTH: usize = PADDED_CHUNK_SIZE * CHUNK_HEIGHT * PADDED_CHUNK_SIZE;
